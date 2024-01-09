3f6a2a57b1c100035cfa37d09dd2299de8562287
nielash <nielronash@gmail.com>
2024-01-09 10:54:40 -0500

    genfilters: fix build error on unsupported platforms

--- cmd/genfilters/genfilters_unsupported.go
+++ cmd/genfilters/genfilters_unsupported.go
@@ -5,3 +5,18 @@
 // +build plan9 js
 
 package genfilters
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+
+	"github.com/rclone/rclone/fs"
+)
+
+// this is just here to prevent build errors on unsupported platforms
+
+// GenFilters is the main entry point. It shows a navigable tree view of the current directory and generates filters.
+func GenFilters(ctx context.Context, f fs.Fs, infile, outfile string) error {
+	return fmt.Errorf("genfilters command is not supported on your platform (%v)", runtime.GOOS)
+}
